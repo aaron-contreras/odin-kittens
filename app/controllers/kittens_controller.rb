9ae331da595a2c55ba9e4bf76a3d41d0b7e95753
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-30 15:41:56 -0500

    Add create and update actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -14,7 +14,12 @@ class KittensController < ApplicationController
   end
   
   def create
-    
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to @kitten, success: 'Kitten succesfully created.'
+    else
+      render :new, alert: 'An error occured while creating this kitten.' 
+    end  
   end
 
   def edit
@@ -22,10 +27,21 @@ class KittensController < ApplicationController
   end
 
   def update
-
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, success: 'Kitten succesfully updated.'
+    else
+      render :edit, alert: 'An error occured while udpating this kitten.'
+    end
   end
   
   def destroy
     
+  end
+  
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
