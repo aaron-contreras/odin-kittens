1e63fd60c36e1039725ec40906a16929aa504b1b
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-30 15:02:38 -0500

    Add sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Kitten.delete_all
+
+CUTENESS_LEVELS = %w[very incredibly astronomically]
+SOFTNESS_LEVELS = %w[silky fluffy puffy]
+
+100.times do
+  Kitten.create!(name: Faker::Creature::Cat.name,
+                 age: rand(1..20),
+                 cuteness: CUTENESS_LEVELS.sample,
+                 softness: SOFTNESS_LEVELS.sample)
+end
