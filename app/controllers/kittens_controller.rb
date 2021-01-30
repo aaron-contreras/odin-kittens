19997288b1ea7a7ae48af5deab589e078a4153c6
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-30 14:42:23 -0500

    Add kittens model and controller

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class KittensController < ApplicationController
+end
---
+++ db/migrate/20210130193927_create_kittens.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Creates the kittens model and database table
+class CreateKittens < ActiveRecord::Migration[6.1]
+  def change
+    create_table :kittens do |t|
+      t.string :name
+      t.integer :age
+      t.integer :cuteness
+      t.integer :softness
+
+      t.timestamps
+    end
+  end
+end
